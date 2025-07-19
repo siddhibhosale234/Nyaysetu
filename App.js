269d3c63d7dd232bf907a0e4d32be5d5c39b5204
Siddhi Bhosale <siddhibhosale234@gmail.com>
2025-07-19 11:29:44 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
-
-
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import {Disclaimer} from './Index.jsx'
+import {RoleSelection} from './RoleSelection.jsx'
+import {Signup} from './signupPage/Signup'
+import {Login} from './LoginForm'
+import {LawyerRegistration} from './LawyerRegistration'
+import {LawyerRegistration1} from './LawyerRegistration1'
 function App() {
   return (
     <>
-      <Disclaimer />
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Disclaimer/>}></Route>
+          <Route path="/choose" element={<RoleSelection/>}></Route>
+          <Route path="/signup" element={<Signup/>}></Route>
+          <Route path="/login" element={<Login/>}></Route>
+          <Route path="/lawyer1" element={<LawyerRegistration/>}></Route>
+          <Route path="/lawyer2" element={<LawyerRegistration1/>}></Route>
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
